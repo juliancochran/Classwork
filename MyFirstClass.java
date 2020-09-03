facccb66b2388e51481ee2c7876924366055f18d
Julian Cochran <julian.p.cochran@gmail.com>
2020-09-03 13:16:40 -0400

    Add files via upload
    Upload of first Java File

---
+++ MyFirstClass.java
@@ -0,0 +1,19 @@
+public class MyFirstClass {
+    public static void main(String args[])  {
+        String name = "Julian";
+        System.out.println("Dark theme is for the cool kids");
+    }
+}
+
+/*
+
+if __name__ == "__main__":
+    main()
+
+    if test > 5:
+        print('hello world')
+
+int main()  {
+
+}
+ */
